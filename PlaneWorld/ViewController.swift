0db61cd47b733d17c59d680d79bf5df48255115b
khan019950 <fymcabatcha2016@gmail.com>
2019-07-03 08:35:32 +0530

    initial commit

--- PlaneWorld/ViewController.swift
+++ PlaneWorld/ViewController.swift
@@ -12,64 +12,66 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    private var isAlertVisible: Bool = false
+    
+    
+    @IBOutlet weak var sceneLabel: UILabel!
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
+        self.sceneLabel.text = "Loading configuration, Please wait"
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        configuration.planeDetection = [.horizontal, .vertical]
+        sceneView.debugOptions = [ ARSCNDebugOptions.showFeaturePoints ]
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
+
     
     func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+        showAlert(withTitle: "Uable to start session!", withMsg: error.localizedDescription, isErr: true)
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
         
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    func showAlert(withTitle title : String, withMsg msg: String, isErr: Bool) -> Void {
+        
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default, handler: { (UIAlertAction) -> Void in
+            self.isAlertVisible = false
+        })
+        alert.addAction(alertAction)
+        if isErr {
+            let retryAction = UIAlertAction(title: "Retry", style: .default, handler: { (UIAlertAction) -> Void in
+                self.isAlertVisible = false
+                //TODO RETRY
+            })
+            alert.addAction(retryAction)
+        }
+        isAlertVisible = true
+        present(alert, animated: true)
     }
 }
